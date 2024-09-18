bbb24fb5a18ca8b4f23fb8381b853c88ed23c670
fbonc <felipe.bonchristiano@outlook.com>
2024-09-18 21:45:46 +0100

    added full country names to sidebar

--- sidebar.js
+++ sidebar.js
@@ -1,5 +1,27 @@
+window.onload = function() {
+    loadCountryNames();
+};
+
+let countryNamesMap = {};
+
+function loadCountryNames() {
+
+    return fetch('codes.json')
+        .then(response => response.json())
+        .then(data => {
+            data.Code.forEach(country => {
+                countryNamesMap[country['@value']] = country.Description['#text'];
+            });
+        })
+        .catch(error => console.error('Error loading country names:', error));
+
+}
 
 
+
+
+
+const colours = ['#ffd100', '#c3dfe3', '#a63c06', "hsl(220, 70%, 80%)", "hsl(220, 70%, 70%)","hsl(220, 70%, 60%)","hsl(220, 70%, 50%)","hsl(220, 70%, 40%)", '#343332'];
 
 function showSidebar(countryName, countryCode, data) { 
     console.log(data)
@@ -8,7 +30,7 @@ function showSidebar(countryName, countryCode, data) {
 
     const flagUrl = `https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/${countryCode.toLowerCase()}.svg`;
 
-    let dataContent = '<ol style="line-height: 2; padding: 35px; padding-top: 5px; font-size: 14px;">';
+    let dataContent = '<ol style="line-height: 2; padding-left: 20px; padding-top: 5px; font-size: 14px;">';
 
     const labels = [];
     const values = [];
@@ -16,38 +38,51 @@ function showSidebar(countryName, countryCode, data) {
 
     let otherTotal = 0;
     let dataAvailable = false;
+    let colour;
 
     if (Array.isArray(data) && data.length > 0) {
         for (let i = 0; i < data.length; i++) {
-            dataContent += `<li><span style="font-size: 14px">${data[i].country} exports:</span> <span style="color: #007da3; font-size: 14px;">$${data[i].exports.toLocaleString()}</span></li>`;
+            dataAvailable = true;
+            colour = '#b5b1ae';
+
+            const fullCountryName = countryNamesMap[data[i].country] || data[i].country;
 
             if (i < 8) {
                 labels.push(data[i].country);
                 values.push(Math.round(data[i].exports));
+
+                colour = colours[i];
             } else if (i >= 8) {
                 otherTotal += data[i].exports;
             }
 
-            dataAvailable = true;
+            dataContent += `<li><span style="font-size: 14px">${fullCountryName} (${data[i].country}) exports:</span> <span style="color: ${colour}; font-size: 14px;">$${data[i].exports.toLocaleString()}</span></li>`;
+
+            
+
+            
             
         }
     } else {
-        dataContent += '<span style="color: red">No trade data available at this time. Please try agian later.</span>';
+        dataContent += '<span style="color: red">No trade data available at this time. Please try again later.</span>';
         dataAvailable = false;
     }
     dataContent += '</ol>';
 
-    labels.push("Other Countries");
+    labels.push("Others");
     values.push(otherTotal);
 
 
     sidebarContent.innerHTML = `
+    <div class="country-header-container">
     <h2 class="country-header">
     ${countryName}
-    <img src="${flagUrl}" alt="Flag of ${countryName}" style="width: 40px; height: 20px; margin-left: 10px;">
+    <img src="${flagUrl}" alt="Flag of ${countryName}" style="width: 40px; height: 20px; margin-left: 0px;">
     </h2>
+    </div>
     <canvas id="pieChart" style="width:100%; height: 325px; padding-bottom: 50px"></canvas>
-    <span style="font-size: 15px">Partner countries ranked by total export value (USD):</span>
+    <hr style="margin-top: -20px; margin-bottom: 20px">
+    <span style="font-size: 15px; font-weight: 700;">Partner countries ranked by total export value (USD):</span>
     ${dataContent}`;
 
     sidebar.classList.add('active');
@@ -72,37 +107,28 @@ document.getElementById('closeBtn').addEventListener('click', function() {
 
 function createPieChart(labels, values) {
 
-
-    const dynamicColours = generateColors(values.length - 4)
-
     new Chart("pieChart", {
-        type: 'pie',
+        type: 'doughnut',
         data: {
             labels: labels,
             datasets: [{
-                backgroundColor: ['#ffd100', '#b5b5b5', '#a63c06', ...dynamicColours, '#343332'],
-                data: values
+                backgroundColor: colours,
+                data: values,
+                borderWidth: 0,
             }]
         },
         options: {
             title: {
                 display: true,
-                text: 'Export Distribution'
+                text: 'Export Distribution',
+                padding: 26,
+                fontSize: 20,
+            },
+            legend: {
+                display: false,
             }
+
         }
     }); 
-}
-
-
-function generateColors(count) {
-    const colors = []
-    const baseHue = 220;
-    const saturation = 70;
-
-    for (let i = 0; i < count; i++) {
-        const lightness = 70 - (i * (50 / count));
-        colors.push(`hsl(${baseHue}, ${saturation}%, ${lightness}%)`);
-    }
-    return colors;
 }
 
